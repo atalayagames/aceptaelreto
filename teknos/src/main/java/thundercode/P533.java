6a0eccde7ca2d4ed278ee6867342c53f1d90f307
DaniT45Pro <60744074+DaniT45Pro@users.noreply.github.com>
2021-04-15 16:11:27 +0200

    Create P533.java

---
+++ teknos/src/main/java/thundercode/P533.java
@@ -0,0 +1,28 @@
+package thundercode;
+
+//La botella ganadora
+
+import java.util.Scanner;
+
+public class P533 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int times = sc.nextInt();
+        int limitASuperar = 0, numero = 0, contador = 0, index = 0;
+        for (int i = 0; i < times; i++) {
+            limitASuperar = sc.nextInt() * 8;
+            while ((numero = sc.nextInt()) != 0) {
+                index++;
+                contador += numero;
+                if (contador >= limitASuperar) {
+                    sc.nextLine();
+                    break;
+                }
+            }
+            System.out.println(contador >= limitASuperar ? index : "SIGAMOS RECICLANDO" );
+            index = 0;
+            contador = 0;
+        }
+        sc.close();
+    }
+}
