20d798ac6771645d7e29af6c376bdacf92f0c314
DaniT45Pro <60744074+DaniT45Pro@users.noreply.github.com>
2021-04-15 22:55:52 +0200

    Update P509.java

--- teknos/src/main/java/thundercode/P509.java
+++ teknos/src/main/java/thundercode/P509.java
@@ -8,23 +8,24 @@ import java.util.Scanner;
 
 public class P509 {
     public static void main(String[] args) {
-        int columnes, files, altitud, resultados, minB = 2;
+        int columnes, files, altitud, resultados, minB;
         ArrayList<Integer> minimoBateria = new ArrayList<>();
         final Scanner scanner = new Scanner(System.in);
         while (scanner.hasNext()) {
             files = scanner.nextInt();
             columnes = scanner.nextInt();
             for (int i = 0; i < files; i++) {
+                resultados = 2;
+                minB = 2;
                 for (int j = 0; j < columnes; j++) {
-                    minB = 2;
-                    resultados = 0;
                     altitud = scanner.nextInt();
                     resultados += altitud;
-                    if (resultados < 2 && altitud < 0)
+                    if (resultados < 2 && altitud < 0) {
                         while (altitud < 0) {
                             minB++;
                             altitud++;
                         }
+                    }
                 }
                 minimoBateria.add(i, minB);
             }
