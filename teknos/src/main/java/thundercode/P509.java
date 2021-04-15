33671de02e05d52ce151d103c66a276e08409d45
DaniT45Pro <60744074+DaniT45Pro@users.noreply.github.com>
2021-04-15 18:54:59 +0200

    1SolaArrayList

--- teknos/src/main/java/thundercode/P509.java
+++ teknos/src/main/java/thundercode/P509.java
@@ -10,28 +10,26 @@ public class P509 {
     public static void main(String[] args) {
         int columnes, files;
         Scanner scanner = new Scanner(System.in);
-        ArrayList<Integer> minimoBateria = new ArrayList<>(), resultados = new ArrayList<>(), altitud = new ArrayList<>();
-        while (scanner.hasNext()) {
+        ArrayList<Integer> minimoBateria = new ArrayList<>();
+        while (scanner.hasNextInt()) {
             columnes = scanner.nextInt();
             files = scanner.nextInt();
             for (int i = 0; i < files; i++) {
-                resultados.add(i, 2);
+                int resultados = 2;
                 minimoBateria.add(i, 2);
                 for (int j = 0; j < columnes; j++) {
-                    altitud.add(j, scanner.nextInt());
-                    resultados.set(i, resultados.get(i) + altitud.get(j));
-                    if (resultados.get(i) < 2 && altitud.get(j) < 0)
-                        while (altitud.get(j) < 0) {
+                    int altitud = scanner.nextInt();
+                    resultados += altitud;
+                    if (resultados < 2 && altitud < 0)
+                        while (altitud < 0) {
                             minimoBateria.set(i, minimoBateria.get(i) + 1);
-                            altitud.set(j, altitud.get(j) + 1);
+                            altitud++;
                         }
                 }
             }
             Collections.sort(minimoBateria);
             System.out.println(minimoBateria.get(0));
             minimoBateria.clear();
-            resultados.clear();
-            altitud.clear();
         }
         scanner.close();
     }
