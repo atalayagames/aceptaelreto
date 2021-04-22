445cda658b6ddc0b285e7c9eb9162b96cede9747
DaniT45Pro <60744074+DaniT45Pro@users.noreply.github.com>
2021-04-22 17:32:00 +0200

    Update P143.java

--- teknos/src/main/java/thundercode/P143.java
+++ teknos/src/main/java/thundercode/P143.java
@@ -2,8 +2,21 @@ package thundercode;
 
 //Leyendo el diccionario
 
+import java.sql.Date;
+import java.text.SimpleDateFormat;
+
+import java.util.Scanner;
+
 public class P143 {
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+        int mult, num, acum;
+        while((mult = sc.nextInt()) != 0 && (acum = 0) == 0){
+            while((num = sc.nextInt()) != 0){
+                acum += num * mult;
+            }
+            System.out.println(new SimpleDateFormat("HH:mm:ss").format(new Date((acum * 1000) - 3600000)));
+        }
+        sc.close();
     }
 }
