e6d9a2f6a36761910e82ef305209720ca57f8819
DaniT45Pro <60744074+DaniT45Pro@users.noreply.github.com>
2021-04-22 18:10:05 +0200

    Update P577.java

--- teknos/src/main/java/thundercode/P577.java
+++ teknos/src/main/java/thundercode/P577.java
@@ -8,12 +8,16 @@ public class P577 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int times = sc.nextInt(), primer, segon, tercer;
+        int[] array = new int[times];
         for (int i = 0; i < times; i++) {
             primer = sc.nextInt();
             segon = sc.nextInt();
             tercer = sc.nextInt();
+            array[i] = ((tercer - segon) >= primer ? tercer - segon : tercer - primer + 1);
+        }
+        for (int i = 0; i < array.length; i++) {
+            System.out.println(array[i]);
         }
         sc.close();
     }
-    
 }
