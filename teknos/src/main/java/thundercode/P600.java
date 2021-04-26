7213b6d7e3fa1f662ebb519f88e18401ff7b9107
DaniT45Pro <60744074+DaniT45Pro@users.noreply.github.com>
2021-04-26 19:38:20 +0200

    Create P600.java

---
+++ teknos/src/main/java/thundercode/P600.java
@@ -0,0 +1,28 @@
+package thundercode;
+
+//Puntos de silla
+
+import java.util.Scanner;
+
+public class P600 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int files, columnes;
+        while((files = sc.nextInt()) != 0 && (columnes = sc.nextInt()) != 0){
+            int[][] matriu = new int[files][columnes];
+            for (int i = 0; i < files; i++) {
+                for (int j = 0; j < columnes; j++) matriu[i][j] = sc.nextInt();
+            }
+            System.out.println(columnaValida(0, matriu) && columnaValida(columnes - 1, matriu) && filaValida(0, matriu) && filaValida(files - 1, matriu) ? "SI" : "NO");
+        }
+        sc.close();
+    }
+    public static boolean filaValida(int index, int[][] matriu) {
+        for (int i = 0; i < matriu[index].length; i++) if(matriu[index][0] > matriu[index][i]) return false;
+        return true;
+    }
+    public static boolean columnaValida(int index, int[][] matriu) {
+        for (int i = 0; i < matriu.length; i++) if(matriu[0][index] < matriu[i][index]) return false;
+        return true;
+    }
+}
