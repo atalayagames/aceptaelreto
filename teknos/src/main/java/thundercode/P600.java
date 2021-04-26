2d5690bf52aa8e705d36ad8ee2283adf0d06ffe0
DaniT45Pro <60744074+DaniT45Pro@users.noreply.github.com>
2021-04-26 19:59:26 +0200

    Update P600.java

--- teknos/src/main/java/thundercode/P600.java
+++ teknos/src/main/java/thundercode/P600.java
@@ -8,12 +8,14 @@ public class P600 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int files, columnes;
+        int[][] matriu;
         while((files = sc.nextInt()) != 0 && (columnes = sc.nextInt()) != 0){
-            int[][] matriu = new int[files][columnes];
+            matriu = new int[files][columnes];
             for (int i = 0; i < files; i++) {
                 for (int j = 0; j < columnes; j++) matriu[i][j] = sc.nextInt();
             }
-            System.out.println(columnaValida(0, matriu) && columnaValida(columnes - 1, matriu) && filaValida(0, matriu) && filaValida(files - 1, matriu) ? "SI" : "NO");
+            boolean temp = columnaValida(0, matriu) && columnaValida(columnes - 1, matriu) && filaValida(0, matriu) && filaValida(files - 1, matriu);
+            System.out.println(temp ? "SI" : "NO");
         }
         sc.close();
     }
