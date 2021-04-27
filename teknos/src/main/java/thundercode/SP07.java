74f301e07b3d6b9853787b0917323ba8e8fed72d
DaniT45Pro <60744074+DaniT45Pro@users.noreply.github.com>
2021-04-27 20:44:21 +0200

    Update SP07.java

--- teknos/src/main/java/thundercode/SP07.java
+++ teknos/src/main/java/thundercode/SP07.java
@@ -1,7 +1,31 @@
 package thundercode;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class SP07 {
     public static void main(String[] args) {
-        Scanner sc = new Scanner 
+        Scanner sc = new Scanner(System.in);
+        int count;
+        ArrayList<Integer> r;
+        ArrayList<Integer> a;
+        ArrayList<Integer> v;
+        String linia;
+        while(sc.hasNext()){
+            r = new ArrayList<>();
+            a = new ArrayList<>();
+            v = new ArrayList<>();
+            int ir = 0;
+            int ia = 0;
+            int iv = 0;
+            linia = sc.nextLine();
+            count = 0;
+            for (int i = 0; i < linia.length(); i++) {
+                if(r.size() > 0){
+                    if(r.get(ir))
+                }
+            }
+            System.out.println(count);
+        }
     }
 }
